4d34b0f907f7357fa88aeb1f5954498d4e37690f
Alvaro Picatoste <apicatoste@gmail.com>
2018-04-07 19:31:56 +0200

    Probando Commit desde PyCharm

--- pec3.py
+++ pec3.py
@@ -19,3 +19,33 @@ def contar_palabras_y_espacios(texto):
 texto = "Hola 1 2 3+1 n "
 string_split = contar_palabras_y_espacios(texto)
 
+# Masas atomicas
+
+masas = {'H': 1.007825, 'C': 12.01, 'O': 15.9994, 'N': 14.0067, 'S': 31.972071, 'P': 30.973762}
+
+def calcula_masa_atomica(molecula):
+    """
+    Calcula la masa atomica de una molecula
+    """
+    masa = 0.0
+
+    atomos_molecula = molecula.split("-")  # type: object
+
+    for ii in atomos_molecula:
+        identificador_atomo = list(ii)[0]
+        cuantificador_atomo = ii[1:]
+        if cuantificador_atomo == '':
+            cuantificador_atomo = "1"
+        cuantificador_atomo = int(cuantificador_atomo)
+        masa_atomo = masas[identificador_atomo]
+        masa_asociada_atomo = cuantificador_atomo * masa_atomo
+        masa = masa + masa_asociada_atomo
+        # print identificador_atomo, cuantificador_atomo, masa_atomo, masa_asociada_atomo
+
+    return masa
+
+
+print calcula_masa_atomica('C13-H18-O2')
+print calcula_masa_atomica('C8-H10-N4-O2')
+print calcula_masa_atomica('C20-H25-N3-O')
+print calcula_masa_atomica('C20-H10-O2-P2-S')
